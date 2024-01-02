e43d191aee39cb87fcbad16f7e935746af4d425d
肖永常 <739759506@qq.com>
2024-01-02 16:13:50 +0800

    gitignore

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+function Father() {}
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+Father.prototype.lastName = "Xiao";
 
-setupCounter(document.querySelector('#counter'))
+function Son() {}
+
+function inherit(Target, Origin) {
+  function F() {}
+
+  const f = new F();
+
+  Target.prototype = new F();
+
+  F.prototype = Origin.prototype;
+
+  console.log("f", f.__proto__);
+
+  Target.prototype.constructor = Target;
+}
+
+inherit(Son, Father);
+
+window.son = new Son();
+
+console.log("son", son, son.lastName);
